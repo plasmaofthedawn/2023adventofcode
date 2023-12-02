c306b6b20008ebfb5894b9685d710e094a16a825
plasmaofthedawn <plasmaofthedawn@gmail.com>
2023-12-01 23:55:59 -0500

    housekeeping

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 day1part1:
-	fpc src/day1/part1.p -obuild/day1part1
+	fpc src/day1/part1.pas -obuild/day1part1
 	@echo
 	./build/day1part1
 
 day1part2:
-	fpc src/day1/part2.p -obuild/day1part2
+	fpc src/day1/part2.pas -obuild/day1part2
 	@echo
 	./build/day1part2
 
--- src/day1/part2.pas
+++ src/day1/part2.pas
@@ -1,15 +1,20 @@
-program HelloWorld;
-uses crt;
+program day2part2;
+uses sysutils;
 
 var
 file_: text;
 start: string;
 c: char;
 first, last: char;
-sum, value: int32;
+value, sum: int32;
+names: array [0..9] of string =  ('zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine');
+numbers: array [0..9] of char = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+i, j: int32;
+s: string;
 
 (* Here the main program block starts *)
 begin
+
     { open inp.txt for reading }
     assign(file_, 'resources/day1.txt');
     reset(file_);
@@ -23,14 +28,30 @@ begin
 
         { read a line from the file }
         readln(file_, start);
-
+        
         { fill in placeholders }
         first := 'a';
         last := 'a';
 
         { for each characters }
-        for c in start do
+        for i := 1 to length(start) do
         begin
+
+            c := start[i];
+
+            { check for text number }
+            for j := 0 to 9 do
+            begin
+                { compare the substring with the name of a number }
+                s := copy(start, i, length(names[j]));
+                if (comparestr(s, names[j]) = 0) then
+                begin
+                    { if it matches then this is a number }
+                    c := numbers[j];
+                    break;
+                end;
+            end;
+            
             { if it is a number }
             if (('0' <= c) and (c <= '9')) then
             begin
@@ -39,16 +60,16 @@ begin
                 { if first hasn't been set, then set it as a number}
                 if (first = 'a') then
                     first := c
-            end;
+            end;        
         end;
 
         { calculate the actual value of the number and add it to the sum }
-        val(concat(first, last), value);
+        val(first + last, value);
         sum := sum + value;
-        
+
     end;
 
-    { print out that beautiful sum}
+    { print out that beautiful sum }
     writeln(sum);
-    
+
 end.
